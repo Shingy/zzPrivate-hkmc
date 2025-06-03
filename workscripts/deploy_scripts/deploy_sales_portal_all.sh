f5e1287396c0cd2243b8b2c2d994c172024efd4a
Shingy Lee <9485147@ict-companion.com>
2025-06-03 17:36:19 +0000

    deploy scripts modify

--- workscripts/deploy_scripts/deploy_sales_portal_all.sh
+++ workscripts/deploy_scripts/deploy_sales_portal_all.sh
@@ -10,14 +10,14 @@ MARGE="\033[0;35m"   # font color : margenta
 CYAN_="\033[0;36m"   # font color : cyan
 WHITE="\033[0;37m"   # font color : white / light gray
 
-BG_BLACK="\033[0;30m"   # background color : black
-BG_RED__="\033[0;31m"   # background color : red
-BG_GREEN="\033[0;32m"   # background color : green
-BG_YELLO="\033[0;33m"   # background color : yellow
-BG_BLUE_="\033[0;34m"   # background color : blue
-BG_MARGE="\033[0;35m"   # background color : margenta
-BG_CYAN_="\033[0;36m"   # background color : cyan
-BG_WHITE="\033[0;37m"   # background color : white / light gray
+BG_BLACK="\033[0;40m"   # background color : black
+BG_RED__="\033[0;41m"   # background color : red
+BG_GREEN="\033[0;42m"   # background color : green
+BG_YELLO="\033[0;43m"   # background color : yellow
+BG_BLUE_="\033[0;44m"   # background color : blue
+BG_MARGE="\033[0;45m"   # background color : margenta
+BG_CYAN_="\033[0;46m"   # background color : cyan
+BG_WHITE="\033[0;47m"   # background color : white / light gray
 ############################################
 
 cd /home/user/projects/Workspace_2
@@ -30,12 +30,7 @@ cd sales_portal
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 3)
-if [ "${ZZ_CURR_TARGET}" == "" ];
-then
-    ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 2)
-fi
-echo -e "\n${RED__}end of deploy sales_portal on ${ZZ_CURR_TARGET}\n"
+echo -e "\n${RED__}end of deploy sales_portal on ${ZZ_CURR_SPACE:16}\n"
 
 echo -e "${GREEN}start deploy sales_portal_admin${NOCOLOR}\n"
 
@@ -43,12 +38,7 @@ cd ../sales_portal_admin
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 3)
-if [ "${ZZ_CURR_TARGET}" == "" ];
-then
-    ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 2)
-fi
-echo -e "\n${RED__}end of deploy sales_portal_admin on ${ZZ_CURR_TARGET}\n"
+echo -e "\n${RED__}end of deploy sales_portal_admin on ${ZZ_CURR_SPACE:16}\n"
 
 echo -e "${GREEN}start deploy sales_portal_api${NOCOLOR}\n"
 
@@ -56,12 +46,7 @@ cd ../sales_portal_api
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 3)
-if [ "${ZZ_CURR_TARGET}" == "" ];
-then
-    ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 2)
-fi
-echo -e "\n${RED__}end of deploy sales_portal_api on ${ZZ_CURR_TARGET}\n"
+echo -e "\n${RED__}end of deploy sales_portal_api on ${ZZ_CURR_SPACE:16}\n"
 
 echo -e "${GREEN}start deploy sales_portal_backend${NOCOLOR}\n"
 
@@ -80,12 +65,7 @@ echo -e "start service deploy${NOCOLOR}\n"
 echo -e "\n${BLUE_}end of service deploy${NOCOLOR}\n"
 
 ZZ_CURR_SPACE=`cf target|grep space`
-ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 3)
-if [ "${ZZ_CURR_TARGET}" == "" ];
-then
-    ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 2)
-fi
-echo -e "\n${RED__}end of deploy sales_portal_backend on ${ZZ_CURR_TARGET}\n"
+echo -e "\n${RED__}end of deploy sales_portal_backend on ${ZZ_CURR_SPACE:16}\n"
 
 echo -e "${GREEN}start deploy sales_portal_collaboration${NOCOLOR}\n"
 
@@ -93,12 +73,7 @@ cd ../sales_portal_collaboration
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 3)
-if [ "${ZZ_CURR_TARGET}" == "" ];
-then
-    ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 2)
-fi
-echo -e "\n${RED__}end of deploy sales_portal_collaboration on ${ZZ_CURR_TARGET}\n"
+echo -e "\n${RED__}end of deploy sales_portal_collaboration on ${ZZ_CURR_SPACE:16}\n"
 
 echo -e "${GREEN}start deploy sales_portal_home${NOCOLOR}\n"
 
@@ -106,9 +81,4 @@ cd ../sales_portal_home
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 3)
-if [ "${ZZ_CURR_TARGET}" == "" ];
-then
-    ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 2)
-fi
-echo -e "\n${RED__}end of deploy sales_portal_home on ${ZZ_CURR_TARGET}${NOCOLOR}\n"
+echo -e "\n${RED__}end of deploy sales_portal_home on ${ZZ_CURR_SPACE:16}${NOCOLOR}\n"
--- workscripts/deploy_scripts/deploy_sales_portal_home.sh
+++ workscripts/deploy_scripts/deploy_sales_portal_home.sh
@@ -3,11 +3,6 @@ cd /home/user/projects/zzPrivate/workscripts/deploy_scripts
 ../../commands/cf_login.sh $1 $2
 
 ZZ_CURR_SPACE=`cf target|grep space`
-ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 3)
-if [ "${ZZ_CURR_TARGET}" == "" ];
-then
-    ZZ_CURR_TARGET=$(echo ${ZZ_CURR_SPACE:16}|cut -d'_' -f 2)
-fi
 
 find /home/user/projects/Workspace_2/zzdeploy_logs -name "_home_${ZZ_CURR_SPACE:16}_*.log" -mtime +2 -delete
 ./__deploy_sales_portal_home.sh $1 $2 $3 > /home/user/projects/Workspace_2/zzdeploy_logs/_home_${ZZ_CURR_SPACE:16}_`date -d "+9 hours" +%y%m%d_%H%M%S`.log
