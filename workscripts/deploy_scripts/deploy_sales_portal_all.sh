5cd3879f5f911764293d6e47ef6fbc9a04edeaaa
Shingy Lee <9485147@ict-companion.com>
2025-06-06 16:29:31 +0000

    scripts modify

--- workscripts/deploy_scripts/deploy_sales_portal_all.sh
+++ workscripts/deploy_scripts/deploy_sales_portal_all.sh
@@ -9,61 +9,61 @@ cd /home/user/projects/Workspace_2
 
 # ./zzcf_login $1 $2
 
-echo -e "\n${GREEN}start deploy sales_portal${NOCOLOR}\n"
+echo -e "\n${GREEN}start deploy sales_portal :: $(generate_datetime 'formatted')${NOCOLOR}\n"
 
 cd sales_portal
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-echo -e "${RED__}end of deploy sales_portal on ${ZZ_CURR_SPACE:16}\n"
+echo -e "${RED__}end of deploy sales_portal on ${ZZ_CURR_SPACE:16} :: $(generate_datetime 'formatted')\n"
 
-echo -e "${GREEN}start deploy sales_portal_admin${NOCOLOR}\n"
+echo -e "${GREEN}start deploy sales_portal_admin :: $(generate_datetime 'formatted')${NOCOLOR}\n"
 
 cd ../sales_portal_admin
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-echo -e "${RED__}end of deploy sales_portal_admin on ${ZZ_CURR_SPACE:16}\n"
+echo -e "${RED__}end of deploy sales_portal_admin on ${ZZ_CURR_SPACE:16} :: $(generate_datetime 'formatted')\n"
 
-echo -e "${GREEN}start deploy sales_portal_api${NOCOLOR}\n"
+echo -e "${GREEN}start deploy sales_portal_api :: $(generate_datetime 'formatted')${NOCOLOR}\n"
 
 cd ../sales_portal_api
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-echo -e "${RED__}end of deploy sales_portal_api on ${ZZ_CURR_SPACE:16}\n"
+echo -e "${RED__}end of deploy sales_portal_api on ${ZZ_CURR_SPACE:16} :: $(generate_datetime 'formatted')\n"
 
-echo -e "${GREEN}start deploy sales_portal_backend${NOCOLOR}\n"
+echo -e "${GREEN}start deploy sales_portal_backend :: $(generate_datetime 'formatted')${NOCOLOR}\n"
 
 cd ../sales_portal_backend
 
-echo -e "${BLUE_}start cds deploy${NOCOLOR}\n"
+echo -e "${BLUE_}start cds deploy :: $(generate_datetime 'formatted')${NOCOLOR}\n"
 
 ./zzcds_deploy $1 $2 $3
 
-echo -e "${BLUE_}end of cds deploy\n"
+echo -e "${BLUE_}end of cds deploy :: $(generate_datetime 'formatted')\n"
 
-echo -e "start service deploy${NOCOLOR}\n"
+echo -e "start service deploy :: $(generate_datetime 'formatted')${NOCOLOR}\n"
 
 ./zzdeploy $1 $2 $3
 
-echo -e "${BLUE_}end of service deploy${NOCOLOR}\n"
+echo -e "${BLUE_}end of service deploy :: $(generate_datetime 'formatted')${NOCOLOR}\n"
 
 ZZ_CURR_SPACE=`cf target|grep space`
-echo -e "${RED__}end of deploy sales_portal_backend on ${ZZ_CURR_SPACE:16}\n"
+echo -e "${RED__}end of deploy sales_portal_backend on ${ZZ_CURR_SPACE:16} :: $(generate_datetime 'formatted')\n"
 
-echo -e "${GREEN}start deploy sales_portal_collaboration${NOCOLOR}\n"
+echo -e "${GREEN}start deploy sales_portal_collaboration :: $(generate_datetime 'formatted')${NOCOLOR}\n"
 
 cd ../sales_portal_collaboration
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-echo -e "${RED__}end of deploy sales_portal_collaboration on ${ZZ_CURR_SPACE:16}\n"
+echo -e "${RED__}end of deploy sales_portal_collaboration on ${ZZ_CURR_SPACE:16} :: $(generate_datetime 'formatted')\n"
 
-echo -e "${GREEN}start deploy sales_portal_home${NOCOLOR}\n"
+echo -e "${GREEN}start deploy sales_portal_home :: $(generate_datetime 'formatted')${NOCOLOR}\n"
 
 cd ../sales_portal_home
 ./zzdeploy $1 $2 $3
 
 ZZ_CURR_SPACE=`cf target|grep space`
-echo -e "${RED__}end of deploy sales_portal_home on ${ZZ_CURR_SPACE:16}${NOCOLOR}\n"
+echo -e "${RED__}end of deploy sales_portal_home on ${ZZ_CURR_SPACE:16} :: $(generate_datetime 'formatted')${NOCOLOR}\n"
